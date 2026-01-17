a6d58ad4019425f0c2957a4072e79a362dbc3be1
a3laa wahib <alaawaaheeb@gmail.com>
2026-01-17 13:50:14 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -29,11 +29,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  // This is required to allow the Next.js dev server to accept requests from the
-  // Firebase Studio environment.
-  allowedDevOrigins: [
-      '*.cloudworkstations.dev'
-  ],
 };
 
 export default nextConfig;
